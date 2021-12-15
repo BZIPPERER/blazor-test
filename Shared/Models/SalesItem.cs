3420f4cf095e057dc774dd4760047839b35df611
Benjamin Zipperer <benjamin-zipperer@t-schommer.de>
2021-12-15 21:41:40 +0100

    Final adaption Wednesday

--- Shared/Models/SalesItem.cs
+++ Shared/Models/SalesItem.cs
@@ -1,12 +1,15 @@
 namespace MafiaApplication.Shared.Models
 {
-    /* James Ryan Style */
+    
     // No-Constructor
     public class SalesItem
     {
+
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Location { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
 
     }
 
