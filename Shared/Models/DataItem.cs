c1fa2c8225c3a7bc00b7d99b183d7282f93cfa7f
Benjamin <benjaminz.1@gmx.de>
2021-12-15 13:13:46 +0100

    Hickory/Anderson

---
+++ Shared/Models/DataItem.cs
@@ -0,0 +1,12 @@
+namespace MafiaApplication.Shared.Models
+{
+    /*80 Tage */
+    public class DataItem
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+
+    }
+
+}
